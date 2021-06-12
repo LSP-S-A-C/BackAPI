cf3537dac6855b65dc8b38db81f8eb2609d7f356
BrujitoOz <ozemblem66@gmail.com>
2021-06-11 21:55:07 -0500

    cors

--- userservice/src/main/java/com/userservice/config/SecurityConfig.java
+++ userservice/src/main/java/com/userservice/config/SecurityConfig.java
@@ -24,8 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.cors()
-                //.and()
+                .cors()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
