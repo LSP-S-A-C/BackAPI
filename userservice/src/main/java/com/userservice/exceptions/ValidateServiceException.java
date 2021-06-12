330d2bea21b7c39c7c85f7fdaf437b2278974a9f
Danilotumix <dani96hbo@gmail.com>
2021-06-12 16:37:30 -0500

    Agregadas validaciones extras. Refactoring.

--- userservice/src/main/java/com/userservice/exceptions/ValidateServiceException.java
+++ userservice/src/main/java/com/userservice/exceptions/ValidateServiceException.java
@@ -1,9 +1,14 @@
 package com.userservice.exceptions;
+import com.userservice.utils.PublicEnums;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class ValidateServiceException extends RuntimeException {
     public ValidateServiceException(String message) {
         super(message);
+    }
+
+    public ValidateServiceException(PublicEnums.ExceptionMessagesUser exception) {
+        super(exception.getValue());
     }
 }
--- userservice/src/main/java/com/userservice/validators/UserValidator.java
+++ userservice/src/main/java/com/userservice/validators/UserValidator.java
@@ -1,31 +1,42 @@
 package com.userservice.validators;
 import com.userservice.entity.User;
 import com.userservice.exceptions.ValidateServiceException;
+import com.userservice.utils.PublicEnums;
+
 public class UserValidator {
     public static void validate(User user) {
+        if (user == null) {
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.USER_REQUIRED);
+        }
         if (user.getName() == null || user.getName().trim().isEmpty()) {
-            throw new ValidateServiceException("El nombre es requerido");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.NAME_REQUIRED);
         }
         if (user.getName().length() > 20) {
-            throw new ValidateServiceException("El nombre es muy largo (max 20 caracteres)");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.NAME_TOO_LONG);
         }
         if (user.getName().length() < 3) {
-            throw new ValidateServiceException("El nombre es muy corto (min 3 caracteres)");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.NAME_TOO_SHORT);
         }
         if (user.getPassword() == null || user.getPassword().trim().isEmpty()) {
-            throw new ValidateServiceException("La contraseña es requerida");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.PASSWORD_REQUIRED);
         }
         if (user.getPassword().length() > 20) {
-            throw new ValidateServiceException("La contraseña es muy larga (max 20 caracteres)");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.PASSWORD_TOO_LONG);
         }
         if (user.getPassword().length() < 3) {
-            throw new ValidateServiceException("La contraseña es muy corta (min 3 caracteres)");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.PASSWORD_TOO_SHORT);
         }
         if (user.getEmail() == null || user.getEmail().trim().isEmpty()) {
-            throw new ValidateServiceException("El email es requerido");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.EMAIL_REQUIRED);
+        }
+        if (!user.getEmail().matches("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$")) {
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.EMAIL_INVALID);
         }
         if (user.getPhone() == null || user.getPhone().trim().isEmpty()) {
-            throw new ValidateServiceException("El numero de celular es requerido");
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.PHONE_REQUIRED);
+        }
+        if (user.getPhone().length() != 9 || !user.getPhone().matches("-?\\d+(\\.\\d+)?")){
+            throw new ValidateServiceException(PublicEnums.ExceptionMessagesUser.PHONE_INVALID);
         }
     }
     
